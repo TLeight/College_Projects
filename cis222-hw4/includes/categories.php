3315db8fd75d8d847d154c5dff6518cd6efcdc07
tyler leight <fireraider98@gmail.com>
2024-06-25 13:39:16 -0400

    Delete cis222-hw4 directory

--- cis222-hw4/includes/categories.php
+++
@@ -1,7 +0,0 @@
-<?php
-$categories = [
-    ['id' => 0, 'name' => 'Appetizer'],
-    ['id' => 1, 'name' => 'Main Course'],
-    ['id' => 2, 'name' => 'Dessert'],
-    ['id' => 3, 'name' => 'Beverage'],
-];
