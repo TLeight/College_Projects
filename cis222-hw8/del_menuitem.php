3640861f9581a18503bf7990bb488df9a5fc5774
tyler leight <fireraider98@gmail.com>
2024-06-25 13:44:02 -0400

    Delete cis222-hw8 directory

--- cis222-hw8/del_menuitem.php
+++
@@ -1,7 +0,0 @@
-<?php 
-require_once('../../config.php');
-require_once 'includes/admin_sessions.php'; 
-require_once('includes/functions.php');
-$_SESSION['message'] = 'Delete menu item feature coming soon.';
-header("Location: admin.php");
-?>
