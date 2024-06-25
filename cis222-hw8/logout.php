fb5d8829cfb42929a7068d9c7ed23f1426362581
tyler leight <fireraider98@gmail.com>
2024-06-25 13:34:12 -0400

    Delete cis222-hw8 directory

--- cis222-hw8/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once 'includes/sessions.php';
-logout();  
-$_SESSION['$user_email'] = '';                           // Call logout() to terminate session
-header('Location: index.php');         // Redirect to home page
-exit;
