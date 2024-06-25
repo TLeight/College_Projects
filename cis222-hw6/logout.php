1114c4b147c21212be06200cd7b7dafd620cd5ea
tyler leight <fireraider98@gmail.com>
2024-06-25 13:34:04 -0400

    Delete cis222-hw6 directory

--- cis222-hw6/logout.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once 'includes/sessions.php';
-logout();                             // Call logout() to terminate session
-header('Location: index.php');         // Redirect to home page
