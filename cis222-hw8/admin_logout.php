7ce5e6818e0d6ff4b2b43f6d1d111060938c0a83
tyler leight <fireraider98@gmail.com>
2024-06-25 13:42:36 -0400

    Delete cis222-hw8 directory

--- cis222-hw8/admin_logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once 'includes/admin_sessions.php';
-logout();    
-$_SESSION['user_email'] = "";
-header('Location: admin_login.php');         // Redirect to home page
-exit;
--- cis222-hw8/includes/footer.php
+++
@@ -1,5 +0,0 @@
-    <button><a href="javascript:history.go(-1)">Go Back</a></button>
-    <footer>
-        <p>&copy; 2023 T Leight</p>
-    </footer>
-</body>
